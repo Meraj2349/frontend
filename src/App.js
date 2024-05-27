481cd9cb516b7787e0bd58af6190e029aad90f0f
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2024-05-28 01:04:33 +0600

    chore: Add Bootstrap, React Bootstrap, and React Icons dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Headers from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
-  return <div>Welcome react</div>;
+  return (
+    <>
+      <Headers />
+      <main className="py-3">
+        <h1> Hello from App.js </h1>
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default App;
