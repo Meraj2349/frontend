585f8bf118cdb0999032091da8f1cc7f5ea041e3
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2024-05-28 23:34:26 +0600

    feat: Add HomeScreen component and update Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Headers from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
     <>
@@ -8,7 +10,9 @@ const App = () => {
       <main className="py-3">
         <h1> Hello from App.js </h1>
       </main>
-
+      <Container>
+        <HomeScreen/>
+      </Container>                                                                                            
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
-import {Navbar, Nav,Container} from 'react-bootstrap';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
+import React from "react";
 const Header = () => {
-    return (
-        <header>
-            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-                <Container>
-                    <Navbar.Brand href="/">KroyBkroy</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
-}
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="KroyBkroy" />
+            KroyBkroy
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
